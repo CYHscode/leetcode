81c7fea26f9323c2520f9070291cbed60a5ba6ba
CYHscode <40061961+CYHscode@users.noreply.github.com>
2018-06-20 23:24:58 +0800

    实现strStr()功能
    给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。

--- Implement-strStr().py
+++ Implement-strStr().py
@@ -10,6 +10,6 @@ class Solution:
         if l2 == 0:
             return 0
         for i in range(l1-l2+1):
-            if haystack[i:i+l2] == needle[:]:
+            if haystack[i:i+l2] == needle[:]:  #needle代入1字符串
                 return i
         return -1
